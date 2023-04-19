c89e94e2c9821ec39ec11050798a606c2e59ec0b
Ng Wen Jing <115871755+wcnjing@users.noreply.github.com>
2023-04-19 08:33:36 +0800

    Update Question 1

--- Question1.java
+++ Question1.java
@@ -1,12 +1,5 @@
-public class Question1
-{
-  public static void main(String[] args)
-  {
-    System.out.println("Hello World!");
-    /**
-     * Print "Hello World!"
-     * 
-     * Hint: System.out.println(...)
-     */
-  }
+public class Question1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
