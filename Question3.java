f4c0adb5f6c74ca18d77e1bef972c91f2cd01a46
Ng Wen Jing <115871755+wcnjing@users.noreply.github.com>
2023-04-19 08:58:45 +0800

    Update Question 3

--- Question3.java
+++ Question3.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
-
-public class Question3
-{
-  public static void main(String[] args)
-  {
-    /**
+public class Question3 {
+    public static void main(String[] args) {
+     /**
      * Reads integer entered by the user, multiplies the integer by itself and print out the result.
      * 
      * Hint: in.nextInt() to read integer
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter integer: ");
+    int n = in.nextInt();
+    int ans = n*n;
+    System.out.print("Answer: " +ans);
     
-  }
+    }
 }
