1aced3231b6c2c58c3fe6a3cd89b21e9381b8dce
Ng Wen Jing <115871755+wcnjing@users.noreply.github.com>
2023-04-20 09:53:44 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Question2 {
     public static void main(String[] args) {
     Scanner in = new Scanner(System.in); /** initialise to read input */
-    System.out.print();
+    System.out.println();
     double height = in.nextDouble();
-    System.out.print(); /** ask for input */
+    System.out.println(); /** ask for input */
     double weight = in.nextDouble(); /** read the input */
     double BMI = weight / (height * height); /** calculate BMI */
     System.out.printf("%.1f", BMI );
