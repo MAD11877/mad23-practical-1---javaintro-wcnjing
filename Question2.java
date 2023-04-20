f62825f01c64620c0dc604c88db76945592c1f29
Ng Wen Jing <115871755+wcnjing@users.noreply.github.com>
2023-04-20 09:52:20 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class Question2 {
     public static void main(String[] args) {
     Scanner in = new Scanner(System.in); /** initialise to read input */
-    System.out.print("Enter height(m): ");
+    System.out.print();
     double height = in.nextDouble();
-    System.out.print("Enter weight(kg): "); /** ask for input */
+    System.out.print(); /** ask for input */
     double weight = in.nextDouble(); /** read the input */
     double BMI = weight / (height * height); /** calculate BMI */
-    System.out.printf("BMI is %.1f", BMI );
+    System.out.printf("%.1f", BMI );
     }
 }
